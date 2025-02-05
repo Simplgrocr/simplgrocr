9b23c6997d53e94676aeece965a13e7d6c75325e
tirthyakamaldasgupta <dasguptatirthyakamal@gmail.com>
2025-02-05 03:46:40 +0000

    chore: Added PrimeFlex package. feat: Added index page component. feat: Set the default route to the index page.

--- clients/web/src/app/app.config.ts
+++ clients/web/src/app/app.config.ts
@@ -1,9 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+  ],
 };
--- clients/web/src/app/app.routes.ts
+++ clients/web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './pages/index/index.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    title: 'Simplgrocr',
+    path: '',
+    component: IndexComponent,
+  },
+];
